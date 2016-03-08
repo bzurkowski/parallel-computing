c9edb7beb59fd0a7b0ddb77101d7ba5ed08a4e67
Bartosz Zurkowski <zurkowski.bartosz@gmail.com>
2016-03-08 01:21:20 +0100

    Add logging to benchmark exec scripts

--- lab1/bandwidth.sh
+++ lab1/bandwidth.sh
@@ -1,7 +1,16 @@
+echo "=== Compilation..."
 mpicc -o bandwidth_send bandwidth_send.c
 mpicc -o bandwidth_ssend bandwidth_ssend.c
 mpicc -o bandwidth_bsend bandwidth_bsend.c
 
-mpiexec -machinefile mpihosts -np 2 ./bandwidth_send > bandwidth_send.out
-mpiexec -machinefile mpihosts -np 2 ./bandwidth_ssend > bandwidth_ssend.out
-mpiexec -machinefile mpihosts -np 2 ./bandwidth_bsend > bandwidth_bsend.out
+echo "=== MPI_Send bandwidth benchmark ==="
+mpiexec -np 2 ./bandwidth_send > bandwidth_send.out
+cat bandwidth_send.out
+
+echo "=== MPI_Ssend bandwidth benchmark ==="
+mpiexec -np 2 ./bandwidth_ssend > bandwidth_ssend.out
+cat bandwidth_ssend.out
+
+echo "=== MPI_Bsend bandwidth benchmark ==="
+mpiexec -np 2 ./bandwidth_bsend > bandwidth_bsend.out
+cat bandwidth_bsend.out
--- lab1/latency.sh
+++ lab1/latency.sh
@@ -1,7 +1,16 @@
+echo "=== Compilation..."
 mpicc -o latency_send latency_send.c
 mpicc -o latency_ssend latency_ssend.c
 mpicc -o latency_bsend latency_bsend.c
 
-mpiexec -machinefile mpihosts -np 2 ./latency_send > latency_send.out
-mpiexec -machinefile mpihosts -np 2 ./latency_ssend > latency_ssend.out
-mpiexec -machinefile mpihosts -np 2 ./latency_bsend > latency_bsend.out
+echo "=== MPI_Send latency benchmark ==="
+mpiexec -np 2 ./latency_send > latency_send.out
+cat latency_send.out
+
+echo "=== MPI_Ssend latency benchmark ==="
+mpiexec -np 2 ./latency_ssend > latency_ssend.out
+cat latency_ssend.out
+
+echo "=== MPI_Bsend latency benchmark ==="
+mpiexec -np 2 ./latency_bsend > latency_bsend.out
+cat latency_bsend.out
