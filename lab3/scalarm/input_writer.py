128b0e4342a67f01480c61dea5b79040ada79eab
Bartosz Zurkowski <zurkowski.bartosz@gmail.com>
2016-04-04 18:36:54 +0200

    Split monte carlo pi scalarm executor into separate adapters

---
+++ lab3/scalarm/input_writer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2.6
+import sys, json
+
+# extract input params from json
+data = None
+with open(sys.argv[1], 'rb') as input_json:
+    data = json.load(input_json)
+
+ncores   = data['ncores']
+npoints  = data['npoints']
+scalable = data['scalable']
+
+# write params to input file
+with open('input.txt', 'wb+') as input_file:
+    input_file.write('{0} {1} {2}'.format(ncores, npoints, scalable))
