95469d6905e2107df98bbe2057faa57f05e18342
Bartosz Zurkowski <zurkowski.bartosz@gmail.com>
2016-04-05 04:47:30 +0200

    Add matrix multiplication results calculations and plotting

---
+++ lab4/results/plot_walltime.py
@@ -0,0 +1,37 @@
+import argparse
+import matplotlib.pyplot as plt
+
+from loader   import load_results
+from operator import itemgetter
+from metrics  import plot_metric
+
+if __name__ == '__main__':
+    # parse args
+    parser = argparse.ArgumentParser()
+    parser.add_argument('results_path', action='store', help='path to file containing results in csv format')
+    parser.add_argument('--output',     action='store', help='path to output plot file')
+    args = parser.parse_args()
+
+    # load results
+    results = load_results(args.results_path, grouping_col='ncores')
+    results = sorted(results.iteritems())
+
+    # plot performance
+    plt.figure(figsize=(12, 8), dpi=80)
+
+    plot_opts = {
+        'label': lambda ncores: '%s cores' % ncores,
+        'title': 'Matrix multiplication - performance of parallel computation'
+    }
+    legend_opts = {'loc': 'upper left', 'ncol': 1}
+
+    plot_metric('size', 'time', results,
+        metric_arg_name='Matrix size',
+        metric_val_name='Walltime [s]',
+        plot_opts=plot_opts,
+        legend_opts=legend_opts
+    )
+
+    # plt.show()
+    output_path = args.output if args.output else 'output.png'
+    plt.savefig(output_path, dpi=100)
